18ed6b7fc299c385aed16930b6a3d16a4b4f6776
Brandon J Sotiroff <bsotiroff1@gmail.com>
2019-02-16 15:59:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -28,12 +28,16 @@ const johnLennonFacts = (anArr) => {
 }
 
 const iLoveTheBeatles = (num) => {
-  let hoowMuchILoveTheBeatlesArr = [];
-  
-  do {
+  let howMuchILoveTheBeatlesArr = [];
+  if(num >= 15){
+    howMuchILoveTheBeatlesArr.push('I love the Beatles!');
+  } else {
+      do {
     emptyArr.push('I love the Beatles!')
     num++;
   } while (num < 15)
+  }
+
   
   return howMuchILoveTheBeatlesArr;
 }
